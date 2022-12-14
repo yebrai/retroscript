2f36de22d3bc55585ee4abd43257173d2411dacf
Eduard Canal <eduard.canal.m@gmail.com>
2022-12-14 19:53:24 +0100

    fixed merging errors

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,119 +1,104 @@
 class Game {
+  fondo: HTMLImageElement
+  frames: number
+ // mainCharacter: Ryu
+  isGameOn: boolean
+  score: number
+  x: number
+  y: number
+  w: number
+  h: number
+  direction: string
+  movement: {left: boolean, right: boolean, isJumping: boolean}
+  player: Ken
 
-    fondo: HTMLImageElement
-    frames: number
-   // mainCharacter: Ryu
-    isGameOn: boolean
-    score: number
-    x: number
-    y: number
-    w: number
-    h: number
-    direction: string
-    movement: {left: boolean, right: boolean, isJumping: boolean}
-    player: Ken
-  
-      constructor() {
-          // todas nuestras propiedades o elementos del juego
-          // fondo
-          this.fondo = new Image()
-          this.fondo.src = "../../images/Canvas-Background.png"
+    constructor() {
+        // todas nuestras propiedades o elementos del juego
+        // fondo
+        this.fondo = new Image()
+        this.fondo.src = "../../images/Canvas-Background.png"
+        this.player = new Ken()
+        this.x = 1; // posición en eje x
+        this.y = 0; // posición en eje y
+        this.w = 40; // ancho 
+        this.h = 35; // alto 
+      //  this.mainCharacter = new Ryu() // creando un nuevo obj de la clase pollo
+        this.frames = 0 // aumentará 60 veces por segundo
+        this.isGameOn = true
+        this.direction = ""
+        this.movement = { left: false, right: false, isJumping: false };
 
-          this.player = new Ken()
+        this.score = 0
 
-          this.x = 1; // posición en eje x
-          this.y = 0; // posición en eje y
-          this.w = 40; // ancho 
-          this.h = 35; // alto 
-        //  this.mainCharacter = new Ryu() // creando un nuevo obj de la clase pollo
-
-          this.frames = 0 // aumentará 60 veces por segundo
-          this.isGameOn = true
-
-          this.direction = ""
-
-          this.movement = { left: false, right: false, isJumping: false };
-  
-          this.score = 0
-  
-      }
-
-  
-      drawFondo = () => {
-        //ctx.drawImage(this.fondo, 0, 0, canvas.width, canvas.height)
-        ctx.drawImage(this.fondo, this.x, this.y +100, canvas.width, 336, 0, 0, canvas.width, canvas.height)
-      };
-    
-      gameOver = () => {
-        // detener el juego
-        this.isGameOn = false
-    
-        // ocultar el canvas
-        canvas.style.display = "none"
-    
-        // mostrar la pantalla de fin
-        gameOverScreen.style.display = "flex"
-      }
-
-        //movimiento derecha
-  moveRight = () => {
-
-      this.x = this.x + 1;
-      this.direction = "right";
-    
-  };
-
-  //movimiento izquierda
-    moveLeft = () => {
-    if (this.x > 0) {
-        this.x = this.x - 1;
-      this.direction = "left";
     }
-  };
 
-    moveBackground = () => {
-      if ((this.player.positionX >= (canvas.width / 2)) && this.movement["right"]){
-                 this.moveRight();
-        } else if ((this.player.positionX <= 0) && this.movement["left"]) {
-          this.moveLeft();
-        }
-      }
+    drawFondo = () => {
+      //ctx.drawImage(this.fondo, 0, 0, canvas.width, canvas.height)
+      ctx.drawImage(this.fondo, this.x, this.y +100, canvas.width, 336, 0, 0, canvas.width, canvas.height)
+    };
   
+    gameOver = () => {
+      // detener el juego
+      this.isGameOn = false
   
+      // ocultar el canvas
+      canvas.style.display = "none"
   
+      // mostrar la pantalla de fin
+      gameOverScreen.style.display = "flex"
+    }
+      //movimiento derecha
+moveRight = () => {
+    this.x = this.x + 1;
+    this.direction = "right";
   
+};
+//movimiento izquierda
+  moveLeft = () => {
+  if (this.x > 0) {
+      this.x = this.x - 1;
+    this.direction = "left";
+  }
+};
+
+  moveBackground = () => {
+    if ((this.player.positionX >= (canvas.width / 2)) && this.movement["right"]){
+      this.moveRight();
+} else if ((this.player.positionX <= 0) && this.movement["left"]) {
+this.moveLeft();
+}
+}
+
       // * BONUS 
       drawScore = () => {
-          ctx.font = "30px Arial"
-          // (elTexto, posX, posY)
-          let scoreStr = `Score: ${this.score}`
-          ctx.fillText(scoreStr, canvas.width * 0.4, 50)
-      }
-  
-
-    
-  
-      gameLoop = () => {
-        this.frames = this.frames + 1
-        //console.log(this.frames)
-    
-        // 1. limpiar el canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-        // 2. acciones y movimientos de los elementos
-        this.moveBackground()
-        this.player.animateKen(this.frames, this.movement.right, this.movement.left, this.movement.isJumping)
-        this.player.movingKen(this.movement["right"], this.movement["left"])
-        // 3. dibujado de los elementos
-        this.drawFondo();
-        this.drawScore()
-        this.player.drawKen()
-    
-        // 4. control de la recursion
-        if (this.isGameOn === true) {
-          requestAnimationFrame(this.gameLoop);
-        }
-
-        //delete me
-      };
+        ctx.font = "30px Arial"
+        // (elTexto, posX, posY)
+        let scoreStr = `Score: ${this.score}`
+        ctx.fillText(scoreStr, canvas.width * 0.4, 50)
     }
+
+  
+
+    gameLoop = () => {
+      this.frames = this.frames + 1
+      //console.log(this.frames)
+  
+      // 1. limpiar el canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+      // 2. acciones y movimientos de los elementos
+      this.moveBackground()
+      this.player.animateKen(this.frames, this.movement.right, this.movement.left, this.movement.isJumping)
+      this.player.movingKen(this.movement["right"], this.movement["left"])
+      // 3. dibujado de los elementos
+      this.drawFondo();
+      this.drawScore()
+      this.player.drawKen()
+  
+      // 4. control de la recursion
+      if (this.isGameOn === true) {
+        requestAnimationFrame(this.gameLoop);
+      }
+      //delete me
+    };
+  }
--- src/game/mainCharacter/ken.ts
+++ src/game/mainCharacter/ken.ts
@@ -1,6 +1,4 @@
 
-
-
 class Ken {
   imgWalk: HTMLImageElement
   walk: {
@@ -16,7 +14,6 @@ class Ken {
     w: number[]
     h: number
   }
-
   img: HTMLImageElement
   action: {
     x: number
@@ -24,7 +21,6 @@ class Ken {
     w: number
     h: number
   }
-
   spriteFrame: number
   positionX: number
 
@@ -47,77 +43,61 @@ class Ken {
       w: [46, 42, 64, 40, 80, 50, 40],
       h: 95
     }
-
     this.img = this.imgWalk
-    this.action
+    this.action = this.walk
     //sprite frame counter
     this.spriteFrame = 0
-
-    //position x
-    this.positionX = 0
-  }
-
-  
-
-
-  drawKen = () => {
-    ctx.drawImage(this.img, this.action.x, this.action.y, this.action.w, this.action.h, this.positionX, 200, this.action.w, this.action.h)
-  }
-
-  animateKen = (frames: number, right: boolean, left: boolean, isJumping: boolean) => {
-    if (isJumping) {
-      this.animateKenJumping(frames, isJumping)
-    } else { 
-      this.walk = {
-        x: 0, // posición en eje x
-        y: 0, // posición en eje y
-        w: 49, // ancho
-        h: 87, // alto
+       //position x
+       this.positionX = 0
       }
-      this.animateKenWalking(frames, right, left) 
+      drawKen = () => {
+        ctx.drawImage(this.img, this.action.x, this.action.y, this.action.w, this.action.h, this.positionX, 200, this.action.w, this.action.h)
+      }
+    
+      animateKen = (frames: number, right: boolean, left: boolean, isJumping: boolean) => {
+        if (isJumping) {
+          this.animateKenJumping(frames, isJumping)
+        } else { this.animateKenWalking(frames, right, left) }
+    
+      }
+    
+      animateKenWalking = (frames: number, right: boolean, left: boolean) => {
+        this.img = this.imgWalk
+        this.action = this.walk
+        this.action.w = this.walk.w //! WHHHHHYY???
+        if (frames % 29 === 0 && (right || left)) {
+          this.action.x = this.action.x + this.walk.w
+          console.log("walking x", this.action.x, "sumando", this.action.w)
+        }
+        if (this.walk.x > 196) {
+          this.walk.x = 0
+        }
+      }
+    
+      animateKenJumping = (frames: number, isJumping: boolean) => {
+        this.img = this.imgJump
+        this.action.y = this.jump.y
+        this.action.h = this.jump.h
+        if (frames % 29 === 0) {
+          this.action.x = this.jump.x[this.spriteFrame]
+          this.action.w = this.jump.w[this.spriteFrame]
+          this.spriteFrame = this.spriteFrame + 1
+          console.log("this.spriteFrame", this.spriteFrame)
+        } if (this.spriteFrame > 5) {
+          this.spriteFrame = 0
+        }
+      }
+    
+      movingKen = (right: boolean, left: boolean) => {
+        if (right && (this.positionX < (canvas.width / 2))) {
+          this.positionX = this.positionX + 1;
+        } else if (left && (this.positionX > 0)) {
+          this.positionX = this.positionX - 1;
+        }
+    
+        console.log("this.positionX", this.positionX)
+      }
+      //delete me
+    
+    
     }
-
-  }
-
-
-  animateKenWalking = (frames: number, right: boolean, left: boolean) => {
-    this.img = this.imgWalk
-    this.action  = this.walk
-    if (frames % 29 === 0 && (right || left)) {
-      this.action.x = this.action.x + this.walk.w
-      console.log("walking", this.walk, "action", this.action)
-    }
-    if (this.action.x > 196) {
-      this.action.x = 0
-    }
-  }
-
-  animateKenJumping = (frames: number, isJumping: boolean) => {
-    this.img = this.imgJump
-    this.action.y = this.jump.y
-    this.action.h = this.jump.h
-    if (frames % 29 === 0) {
-      this.action.x = this.jump.x[this.spriteFrame]
-      this.action.w = this.jump.w[this.spriteFrame]
-      this.spriteFrame = this.spriteFrame + 1
-      console.log("this.spriteFrame", this.spriteFrame)
-
-    } if (this.spriteFrame > 5) {
-      this.spriteFrame = 0
-    }
-  }
-
-  movingKen = (right: boolean, left: boolean) => {
-    if (right && (this.positionX < (canvas.width / 2))) {
-      this.positionX = this.positionX + 1;
-    } else if (left && (this.positionX > 0)) {
-
-      this.positionX = this.positionX - 1;
-    }
-
-  
-  }
-  //delete me
-
-
-}
